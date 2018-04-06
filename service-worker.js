55ca4b4c19920c7575961a6145818b434887dece
MathiasMogensen <mathiasgulbaek@hotmail.dk>
2018-04-06 08:49:16 +0200

    cache?

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,13 @@
 self.addEventListener('fetch', function(e){
-
+    event.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+          return cache.addAll(
+            [
+              '/assets/css/main.css',
+              '/assets/js/maps.js',
+              '/index.html'
+            ]
+          );
+        })
+      );
 });
